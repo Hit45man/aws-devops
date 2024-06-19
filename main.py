2a743f013b0dfff73ca868f9529d14950ffd0b16
Rohit Kamble <krohit.kamble@gmail.com>
2024-06-19 12:34:55 +0530

    changed header

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to my first CICD  Application deployed on AWS '
+    return 'Welcome to my first AWS devops project'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
