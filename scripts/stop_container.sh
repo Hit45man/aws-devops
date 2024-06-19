69255b456f16100740eca00fb2c2474bf6fd0bb4
Rohit Kamble <krohit.kamble@gmail.com>
2024-06-19 14:13:27 +0530

    added new script for stop

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Get a list of all running Docker containers
+containers=$(docker ps -aq)
+
+# If there are any containers running
+if [ -n "$containers" ]; then
+  # Stop all running containers
+  docker stop $(docker ps -aq)
+
+  # Delete all stopped containers
+  docker rm $(docker ps -aq -f status=exited)
+
+  echo "All Docker containers stopped and deleted."
+else
+  echo "No Docker containers found."
+fi
